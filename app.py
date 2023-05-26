197d9dcf24e3e28f7b841b554edcb041520c12f7
Shubham <shubham.adep@yahoo.com>
2023-05-26 12:25:46 -0700

    remove made with streamlit labels

--- app.py
+++ app.py
@@ -157,3 +157,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
